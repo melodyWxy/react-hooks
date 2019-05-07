765525598fc334cfe205fb29b856dc4f351d66b3
melodyWxy <44901373+melodyWxy@users.noreply.github.com>
2019-05-08 03:37:45 +0800

    'jsx-babel配置'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('入口');
+
+import React,{Component} from 'react';
+
+function App (){
+    return (
+        <div >
+            123
+        </div>
+    )
+}
+// export default App;
+const root = document.getElementById('root');
+ReactDOM.render(<App />,root);
